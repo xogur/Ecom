3f175df7c38670a36a259951ec02bcbe002e36a2
xogur <snsk656@naver.com>
2025-05-12 18:00:59 +0900

    Entity

--- src/main/java/com/ecommerce/project/model/Category.java
+++ src/main/java/com/ecommerce/project/model/Category.java
@@ -2,11 +2,16 @@ package com.ecommerce.project.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
+// @Entity 는 데이터 베이스와 해당 클래스가 매핑이 되도록 연결
 @Entity(name = "categories")
-
+// name = "categories"는 JPA 내부 이름이고,
+//실제 DB 테이블 이름은 @Table(name = "테이블명")에서 따로 지정할 수 있습니다.
+//만약 @Table이 없다면 기본적으로 클래스 이름이 테이블 이름이 됩니다 (category → category).
 // 카테고리 모델
 public class Category {
 
+    // 기본키
+    // JPA 데이터를 조회/수정 등을 처리할때 기본키를 기준으로 조회하기 때문에 필수
     @Id
     // 카테고리 아이디
     private Long categoryId;
