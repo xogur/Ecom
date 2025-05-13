9cadf6bfdd0dab738469056c347aa96062409790
xogur <snsk656@naver.com>
2025-05-13 19:46:47 +0900

    오토와이어

--- src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -34,7 +34,7 @@ public class CategoryServiceImpl implements CategoryService{
 
     @Override
     public void createCategory(Category category) {
-        category.setCategoryId(nextId++);
+        // category.setCategoryId(nextId++);
         categoryRepository.save(category);
 
     }
@@ -43,17 +43,8 @@ public class CategoryServiceImpl implements CategoryService{
     @Override
     public String deleteCategory(Long categoryId) {
 
-        List<Category> categories = categoryRepository.findAll();
-        // stream은 categories에서 순회하겠다는 의미
-        Category category = categories.stream()
-                // filter는 조건
-                // 람다식으로 각 카테고리 c의 카테고리 아이디를 조회해 입력받은 categoryId와 일치하는것만 추출
-                .filter(c -> c.getCategoryId().equals(categoryId))
-                // 그 중 첫번째 것만 가져옴
-                .findFirst()
-                // 예외처리 - 만약 없다면 ResponseStatusException는 스프링에서 제공하는 hppt 예외처리
-                // NOT_FOUND에 맞는 상태코드를 사용자에게 반환 (404), 메시지는 Resource not found
-                // 예외를 던짐
+        //
+        Category category = categoryRepository.findById(categoryId)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource not found"));
         if (category == null)
             return "Category not found";
@@ -67,25 +58,14 @@ public class CategoryServiceImpl implements CategoryService{
     // 현재 category에는 클라이언트에서 보낸 수정할 데이터가 담겨져 있음
     public Category updateCategory(Category category, Long categoryId) {
 
-        List<Category> categories = categoryRepository.findAll();
-        // Optional은 값이 없을수도 있다는 가정을 명확하게 하기 위해 사용
-        Optional<Category> optionalCategory = categories.stream()
-                .filter(c -> c.getCategoryId().equals(categoryId))
-                .findFirst();
+        // 실패했을때는 실패값을 리턴
+        Category savedCategory = categoryRepository.findById(categoryId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource Not Found"));
 
-        if(optionalCategory.isPresent()){
-            // Optional의 get() 함수는 실제 값을 꺼냄
-            Category existingCategory = optionalCategory.get();
-            // 실제 값이 저장된 existingCategory의 카테고리 이름을 클라이언트에서 보낸 수정할 데이터가 담겨져 있는
-            // category에서 꺼내서 적용
-            existingCategory.setCategoryName(category.getCategoryName());
 
-            // id 필드 같이 DB에서 자동으로 생성해 주는 값은
-            // 저장이 된 후에 생성이 되기 때문에 변수에 save한 후의 결과 객체를 변수에 저장
-            Category savedCategory = categoryRepository.save(existingCategory);
-            return savedCategory;
-        } else {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found");
-        }
+        category.setCategoryId(categoryId);
+        // 성공했을때의 리턴값을 다시 정의하기 위해 재정의
+        savedCategory = categoryRepository.save(category);
+        return savedCategory;
     }
 }
