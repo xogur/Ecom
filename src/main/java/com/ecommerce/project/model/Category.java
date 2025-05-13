9647796004a2336dd3b4256c34eb6e87341feb47
xogur <snsk656@naver.com>
2025-05-13 20:49:28 +0900

    이름 noblank

--- src/main/java/com/ecommerce/project/model/Category.java
+++ src/main/java/com/ecommerce/project/model/Category.java
@@ -6,6 +6,7 @@ import jakarta.persistence.GenerationType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.NotBlank;
 
 // @Entity 는 데이터 베이스와 해당 클래스가 매핑이 되도록 연결
 @Entity(name = "categories")
@@ -26,6 +27,7 @@ public class Category {
     // 해당 필드의 값을 자동으로 생성해줌
     private Long categoryId;
     // 카테고리 이름
+    @NotBlank
     private String categoryName;
 
     // 카테고리 생성자/ 카테고리 아이디와, 이름 변수에 저장
