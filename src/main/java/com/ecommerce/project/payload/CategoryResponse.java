fcdb5a956288c546baa567a861c43eb6a33acdb5
xogur <snsk656@naver.com>
2025-05-29 19:20:01 +0900

    117

--- src/main/java/com/ecommerce/project/payload/CategoryResponse.java
+++ src/main/java/com/ecommerce/project/payload/CategoryResponse.java
@@ -13,4 +13,9 @@ public class CategoryResponse {
     // CategoryDTO를 리스트 형식으로 받아서 content라는 이름으로 묶음
     // content가 상위 컬럼
     private List<CategoryDTO> content;
+    private Integer pageNumber;
+    private Integer pageSize;
+    private Long totalElements;
+    private Integer totalpages;
+    private boolean lastPage;
 }
--- src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -56,7 +56,13 @@ public class CategoryServiceImpl implements CategoryService{
                 .toList();
 
         CategoryResponse categoryResponse = new CategoryResponse();
+
         categoryResponse.setContent(categoryDTOS);
+        categoryResponse.setPageNumber(categoryPage.getNumber());
+        categoryResponse.setPageSize(categoryPage.getSize());
+        categoryResponse.setTotalElements(categoryPage.getTotalElements());
+        categoryResponse.setTotalpages(categoryPage.getTotalPages());
+        categoryResponse.setLastPage(categoryPage.isLast());
         return categoryResponse;
     }
 
