42c601de59cc64ad4afe9b0a46b263d660bfdaf0
xogur <snsk656@naver.com>
2025-06-05 10:07:19 +0900

    144

--- src/main/java/com/ecommerce/project/service/ProductService.java
+++ src/main/java/com/ecommerce/project/service/ProductService.java
@@ -1,4 +1,12 @@
 package com.ecommerce.project.service;
 
+import com.ecommerce.project.model.Product;
+import com.ecommerce.project.payload.ProductDTO;
+import com.ecommerce.project.payload.ProductResponse;
+
 public interface ProductService {
+
+    ProductDTO addProduct(Long categoryId, Product product);
+
+    ProductResponse getAllProducts();
 }
