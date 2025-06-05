72db204f5ef89ebacb2a164630ff4794570f7840
xogur <snsk656@naver.com>
2025-06-05 14:45:50 +0900

    148

--- src/main/java/com/ecommerce/project/service/ProductService.java
+++ src/main/java/com/ecommerce/project/service/ProductService.java
@@ -13,4 +13,8 @@ public interface ProductService {
     ProductResponse searchByCategory(Long categoryId);
 
     ProductResponse searchProductByKeyword(String keyword);
+
+    ProductDTO updateProduct(Long productId, Product product);
+
+    ProductDTO deleteProduct(Long productId);
 }
