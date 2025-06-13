cb366c00fd6eb82382f52a3379f202e2331b6993
xogur <snsk656@naver.com>
2025-06-13 12:42:48 +0900

    182

--- src/main/java/com/ecommerce/project/model/Product.java
+++ src/main/java/com/ecommerce/project/model/Product.java
@@ -35,4 +35,8 @@ public class Product {
     @ManyToOne
     @JoinColumn(name = "category_id")
     private Category category;
+
+    @ManyToOne
+    @JoinColumn(name = "seller_id")
+    private User user;
 }
