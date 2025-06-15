9c14a1615ee35fe5f49b438a8cb10ef74a7cde55
xogur <snsk656@naver.com>
2025-06-15 13:42:40 +0900

    192

--- src/main/java/com/ecommerce/project/security/WebSecurityConfig.java
+++ src/main/java/com/ecommerce/project/security/WebSecurityConfig.java
@@ -24,22 +24,29 @@ import com.ecommerce.project.security.jwt.AuthEntryPointJwt;
 import com.ecommerce.project.security.jwt.AuthTokenFilter;
 import com.ecommerce.project.security.services.UserDetailsServiceImpl;
 
+// 해당 클래스가 설정 파일임을 명시
+// @Bean을 이용해 스프링 컨테이너에 등록 가능
 @Configuration
+// 나 JWT쓸거임
 @EnableWebSecurity
 //@EnableMethodSecurity
 public class WebSecurityConfig {
+
+    // 사용자 정보
     @Autowired
     UserDetailsServiceImpl userDetailsService;
 
     @Autowired
     private AuthEntryPointJwt unauthorizedHandler;
 
+    // 요청된 jwt토큰을 확인하기 위해
     @Bean
     public AuthTokenFilter authenticationJwtTokenFilter() {
         return new AuthTokenFilter();
     }
 
 
+    // 사용자 인증, 로그인을 처리
     @Bean
     public DaoAuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
@@ -51,18 +58,20 @@ public class WebSecurityConfig {
     }
 
 
+    // 인증을 실제로 수행하는 핵심 컴포넌트
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
         return authConfig.getAuthenticationManager();
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
+    public PasswordEncoder   passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
 
 
+    // 보안 필터 체인을 설정하는 메서드
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
--- src/main/java/com/ecommerce/project/security/jwt/AuthEntryPointJwt.java
+++ src/main/java/com/ecommerce/project/security/jwt/AuthEntryPointJwt.java
@@ -15,9 +15,16 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+
+// JWT인증 실패시 발생하는 예외처리 코드
+// 스피링이 해당 코드를 Bean에 등록
 @Component
 public class AuthEntryPointJwt implements AuthenticationEntryPoint {
 
+    // 로그 기록을 위한 로거
+//    로그를 찍을 대상 클래스 이름을 등록하는 코드입니다.
+//        예: AuthEntryPointJwt에서 발생한 로그인지 식별 가능하도록 해줍니다.
+//    로그를 출력할 때 다음과 같이 클래스명이 함께 찍히게 됩니다:
     private static final Logger logger = LoggerFactory.getLogger(AuthEntryPointJwt.class);
 
     @Override
