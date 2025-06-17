85029a506ce6b1e3f8ea29bb60684c5b19c075f7
xogur <snsk656@naver.com>
2025-06-17 15:49:05 +0900

    211

--- src/main/java/com/ecommerce/project/service/CartService.java
+++ src/main/java/com/ecommerce/project/service/CartService.java
@@ -1,7 +1,9 @@
 package com.ecommerce.project.service;
 
 import com.ecommerce.project.payload.CartDTO;
+import java.util.List;
 
 public interface CartService {
     CartDTO addProductToCart(Long productId, Integer quantity);
+    List<CartDTO> getAllCarts();
 }
